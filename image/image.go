7c43dba6724ac554d316f3297f0f131531863cea
Aayush Kumar <code@aayushk.dev>
2026-07-05 00:32:17 +0530

    add gamma correction

--- image/image.go
+++ image/image.go
@@ -3,6 +3,7 @@ package image
 import (
 	"fmt"
 	"io"
+	stdmath "math"
 
 	rtmath "github.com/aayushkdev/rt-go/math"
 )
@@ -11,13 +12,21 @@ func WriteColor(out io.Writer, pixelColor rtmath.Color, samplesPerPixel int) {
 	scale := 1.0 / float64(samplesPerPixel)
 	intensity := rtmath.NewInterval(0, 0.999)
 
-	r := intensity.Clamp(pixelColor.X * scale)
-	g := intensity.Clamp(pixelColor.Y * scale)
-	b := intensity.Clamp(pixelColor.Z * scale)
+	r := intensity.Clamp(linearToGamma(pixelColor.X * scale))
+	g := intensity.Clamp(linearToGamma(pixelColor.Y * scale))
+	b := intensity.Clamp(linearToGamma(pixelColor.Z * scale))
 
 	rByte := int(255.999 * r)
 	gByte := int(255.999 * g)
 	bByte := int(255.999 * b)
 
 	fmt.Fprintf(out, "%d %d %d\n", rByte, gByte, bByte)
+}
+
+func linearToGamma(linearComponent float64) float64 {
+	if linearComponent > 0 {
+		return stdmath.Sqrt(linearComponent)
+	}
+
+	return 0
 }
